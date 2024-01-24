14c55c393a9589ccda0f090beef019f9908998b0
Noah-Goodwin <144268611+Noah-Goodwin@users.noreply.github.com>
2024-01-24 11:35:29 -0800

    started cave level

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -11,16 +11,12 @@ public class PlayerMovement : MonoBehaviour
     private float YDirection;
     private float YVector;
     public bool Overworld;
-    private bool shouldJump;
-    private bool canJump;
     private Vector2 playerInput;
-    public Rigidbody2D rb;
-    public GameObject characters;
+
 
     
     void Start()
     { 
-        //rb = character.GetComponent<Rigidbody2D>();
         WalkingSpeed = 5f;
     }
 
@@ -45,26 +41,11 @@ public class PlayerMovement : MonoBehaviour
             XVector = XDirection * WalkingSpeed * Time.deltaTime;
             transform.position = transform.position + new Vector3(XVector,0,0);
             
-
-
-            
             
         }
         
-        if(canJump && Input.GetKeyDown(KeyCode.W))
-        {
-            canJump = false;
-            shouldJump = true;
-        }
+
     }
     
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            // allow jumping again
-            canJump = true;
-        }
-    }
 
 }
